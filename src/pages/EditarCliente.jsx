23b5ee5683abdccbbf9ccbd0719b33181f0cf2f8
jesusbenagal <jesusguerreropaniagua6@gmail.com>
2022-04-26 17:01:04 +0200

    Comenzando Proyecto

---
+++ src/pages/EditarCliente.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EditarCliente = () => {
+
+    return (
+        <div>
+            <h1>Editar Cliente</h1>
+        </div>
+    )
+}
+
+export default EditarCliente
